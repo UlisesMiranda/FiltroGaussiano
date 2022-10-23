c6c12549ff9033092d3a1cb07b6ad6272ab19675
Ulises Miranda <ulimirandachavez@hotmail.com>
2022-10-23 15:44:50 -0500

    Convolucion aplicada

--- PracticaFiltros/Source.cpp
+++ PracticaFiltros/Source.cpp
@@ -9,34 +9,42 @@
 using namespace cv;
 using namespace std;
 
-double* rellenarMascara(double sigma, int n);
 
-double * rellenarMascara(double sigma, int n) {
+vector<vector<float>> rellenarMascara(int mascSize, float sigma) {
 
-    int i = 0, limite = n / 2;
-    double r, s,z;
+    int limite = (mascSize - 1)/ 2;
+    float r, s,z, gaussResultado;
+    float sum = 0.0;
 
-    double* valores;
-    valores = new double[n * n];
+    vector<vector<float>> mascara(mascSize, vector<float>(mascSize, 0));
 
     s = 2.0 * sigma * sigma;
 
-  
     for (int x = -limite; x <= limite; x++) {
         for (int y = -limite; y <= limite; y++) {
             r = sqrt(x * x + y * y);
             z = (exp(-(r * r) / s)) / (M_PI * s);
-            valores[i] = (exp(-(r * r) / s)) / (M_PI * s);
-            i++;
+            gaussResultado = (exp(-(r * r) / s)) / (M_PI * s);
+            mascara[x + limite][y + limite] = gaussResultado;
+
+            sum += gaussResultado;
+            
+            cout << gaussResultado << endl;
         }
     }
 
-    return valores;
+    for (int i = 0; i < mascSize; i++) {
+        for (int j = 0; j < mascSize; j++) {
+            mascara[i][j] /= sum;
+        }
+    }
+
+    return mascara;
 }
 
-Mat matrizRelleno(int filas, int columnas, int n)
+Mat matrizRelleno(int filas, int columnas, int mascSize)
 {
-    int diferenciaBordes = n - 1;
+    int diferenciaBordes = mascSize - 1;
     Mat matriz(filas + diferenciaBordes, columnas + diferenciaBordes, CV_8UC1);
 
     for (int i = 0; i < filas + diferenciaBordes; i++)
@@ -50,9 +58,9 @@ Mat matrizRelleno(int filas, int columnas, int n)
     return matriz;
 }
 
-Mat copiarImgARelleno(Mat bordes, Mat original, int n)
+Mat copiarImgARelleno(Mat bordes, Mat original, int mascSize)
 {
-    int diferenciaBordes = ((n - 1) / 2);
+    int diferenciaBordes = ((mascSize - 1) / 2);
     int filas = bordes.rows;
     int columnas = bordes.cols;
 
@@ -67,20 +75,64 @@ Mat copiarImgARelleno(Mat bordes, Mat original, int n)
     return bordes;
 }
 
+float convolucionPixel(Mat matrizConBordes, vector<vector<float>> mascara, int mascSize, int x, int y) {
+
+    int limites = (mascSize - 1) / 2;
+
+    int filasImgBordes = matrizConBordes.rows;
+    int columnasImgBordes = matrizConBordes.cols;
+
+    float sumatoriaFiltro = 0.0;
+
+    for (int i = -limites; i <= limites; i++) {
+        for (int j = -limites; j <= limites; j++) {
+
+            float valMascara = mascara[i + limites][j + limites];
+            int coordY = y + j + limites;
+            int coordX = x + i + limites;
+
+            float valImagen = matrizConBordes.at<uchar>(coordY, coordX);
+
+            sumatoriaFiltro += valMascara * valImagen;
+        }
+    }
+
+    return sumatoriaFiltro;
+}
+
+Mat aplicarFiltroImagen(Mat imagenOriginal, Mat matrizConBordes, vector<vector<float>> mascara, int mascSize) {
+    int filas = imagenOriginal.rows;
+    int columnas = imagenOriginal.cols;
+
+    Mat imagenFiltroAplicado(filas, columnas, CV_8UC1);
+    
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++){
+            uchar val = uchar(convolucionPixel(matrizConBordes, mascara, mascSize, i, j));
+            imagenFiltroAplicado.at<uchar>(Point(i, j)) = val;
+        }
+    }
+
+    return imagenFiltroAplicado;
+}
+
+
+ 
+
 int main()
 {
-    int n = 0, filasImagen = 0, columnasImagen = 0;
-    double sigma = 0.0;
+    int mascSize = 0, filasImagen = 0, columnasImagen = 0;
+    float sigma = 0.0;
 
     Mat image;
     char imageName[] = "lena.png";
 
     cout << "Digite el largo que tendra la mascara cuadrada para aplicar en la imagen: ";
-    cin >> n;
+    cin >> mascSize;
     cout << "Digite el valor de 'sigma' para usar en el filtro Gaussiano: ";
     cin >> sigma;
 
-    if ((n % 2) == 0)
+    if ((mascSize % 2) == 0)
     {
         cout << "La mascara debe ser de longitud impar";
         exit(1);
@@ -96,24 +148,24 @@ int main()
 
     cvtColor(image, image, COLOR_BGR2GRAY);
 
-    double* mascara = new double[n * n];
-    mascara = rellenarMascara(sigma, n);
-
-    for (int i = 0; i < n * n; i++) {
-        cout << mascara[i] << endl;
-    }
+    vector<vector<float>> mascara = rellenarMascara(mascSize, sigma);
 
     filasImagen = image.rows;
     columnasImagen = image.cols;
 
-    Mat matrizConBordes = matrizRelleno(filasImagen, columnasImagen, n);
-    matrizConBordes = copiarImgARelleno(matrizConBordes, image, n);
+    Mat matrizConBordes = matrizRelleno(filasImagen, columnasImagen, mascSize);
+    matrizConBordes = copiarImgARelleno(matrizConBordes, image, mascSize);
 
-    namedWindow("NTSC Grayscale Image", WINDOW_AUTOSIZE);
-    imshow("NTSC Grayscale Image", image);
+    Mat imagenFiltroAplicado = aplicarFiltroImagen(image, matrizConBordes, mascara, mascSize);
 
-    namedWindow("Filtro Obtenido", WINDOW_AUTOSIZE);
-    imshow("Filtro Obtenido", matrizConBordes);
+    namedWindow("Imagen original", WINDOW_AUTOSIZE);
+    imshow("Imagen original", image);
+
+    namedWindow("Imagen Bordes Extra", WINDOW_AUTOSIZE);
+    imshow("Imagen Bordes Extra", matrizConBordes);
+
+    namedWindow("Imagen con filtro", WINDOW_AUTOSIZE);
+    imshow("Imagen con filtro", imagenFiltroAplicado);
 
     waitKey(0);
 
